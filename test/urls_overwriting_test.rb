79f13a0fa0cd2b43f33f4e7d4ae5852765bc1bde
E-Max <max@studentify.nl>
2012-09-10 15:25:36 +0300

    Cleaning up Test file

--- test/urls_overwriting_test.rb
+++ test/urls_overwriting_test.rb
@@ -13,11 +13,8 @@ class UrlsOverwritingTest < ActionController::TestCase
     assert_generates("/", :controller => "main", :action => "index")
     ::I18n.locale = :ru
     assert_generates("/", :controller => "main", :action => "index", :locale => false)
-    ::I18n.locale = :ru
     assert_generates("/", :controller => "main", :action => "index", :locale => :en)
-    ::I18n.locale = :ru
     assert_generates("/", :controller => "main", :action => "index", :locale => "en")
-    ::I18n.locale = :ru
     assert_generates("/ru/", :controller => "main", :action => "index")
   end
 
@@ -26,26 +23,4 @@ class UrlsOverwritingTest < ActionController::TestCase
       assert_generates("/", :controller => "main", :action => "index", :locale => :de)
     end
   end
-
-  #  def test_url_test_no_locale
-  #    assert_equal '/', TestApp.url(:index)
-  #  end
-  #
-  #  def test_url_test_with_locale
-  #    assert_equal '/ru/', TestApp.url_with_locale(:index, :locale => "ru")
-  #    assert_equal '/ru/', TestApp.url(:index, :locale => "ru")
-  #    assert_equal '/ru/', TestApp.url_for(:index, :locale => "ru")
-  #    ::I18n.locale = :ru
-  #    assert_equal '/ru/', TestApp.url(:index)
-  #    assert_equal '/', TestApp.url(:index, :locale => "en")
-  #    ::I18n.locale = ::I18n.default_locale
-  #  end
-  #
-  #  def test_url_is_not_registered
-  #    assert_equal '/', TestApp.url_with_locale(:index, :locale => "sdv")
-  #    ::I18n.locale = :ru
-  #    assert_equal '/ru/', TestApp.url(:index, :locale => "sdfsa")
-  #    assert_equal '/ru/', TestApp.url_for(:index, :locale => "sdfsa")
-  #    ::I18n.locale = ::I18n.default_locale
-  #  end
 end
