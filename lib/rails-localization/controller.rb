6978cb70401457cb59d1575bcac8863142bdcf95
E-Max <max.zab87@gmail.com>
2013-12-10 22:18:32 +0400

    How that reappeared here?

--- lib/rails-localization/controller.rb
+++ lib/rails-localization/controller.rb
@@ -4,16 +4,6 @@ module RailsLocalization
       base.class_eval do
         around_filter :process_with_locale
 
-        def url_options_with_locale
-          opts = url_options_without_locale
-          if opts.has_key?(:_path_segments)
-            opts[:_path_segments].merge(@locale_url_options || { locale: nil})
-          else
-            opts.merge(@locale_url_options || { locale: nil})
-          end
-        end
-        alias_method_chain :url_options, :locale
-
         protected
 
         def process_with_locale
